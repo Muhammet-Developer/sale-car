cf7f181558b6fd3760dfb5b9320dd6d29f57e015
muhammet cetinkaya <muhammed_249@outlook.com>
2024-01-26 09:58:52 +0300

    addCar page design completed

--- src/app/addCar/page.tsx
+++ src/app/addCar/page.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import addCar from '@/styles/addCar.module.scss'
 const page = () => {
   return (
-    <div>
-      addCar
+    <div className={addCar['container']}>
+      <div className={addCar['center-div']}>
+        <div >
+          <p>
+            <span>
+              YENİ İLAN
+            </span>  EKLE
+          </p>
+        </div>
+        <div className={addCar['line']}></div>
+        <form>
+          <div className={addCar['form-container']}>
+            <div>
+              <label htmlFor="name">İlan Başlığı</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="name">İlan Kapak Görseli</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="name">Acil mi?</label>
+              <input type="checkbox" />
+            </div>
+            <div>
+              <button>Kaydet</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
